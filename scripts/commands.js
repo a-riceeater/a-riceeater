716682a73f954b030802e2a417220e5e7dabb46e
a-riceeater <elijb12@gmail.com>
2023-10-06 22:55:52 -0700

    projects

--- scripts/commands.js
+++ scripts/commands.js
@@ -47,4 +47,14 @@ export const projects = `
 <u>SyntaxHighlight.js</u> - A JS syntax highlight library, which can highlight a few languages (https://github.com/a-riceeater/SyntaxHiglight.js)
 <br><br>
 <u>2DGame.js</u> - A simple 2D game engine for javascript (https://github.com/a-riceeater/2DGame.js)
+<br><br>
+<u>Nimbus</u> - An IDE powered by Electron (https://github.com/a-riceeater/Nimbus)
+<br><br>
+<u>CMail</u> - An encrypted mail service in which you can send emails via web or command line (https://a-riceeater/CMail) (private)
+<br><br>
+<u>Autocomplete BetterDiscord</u> - A BetterDiscord plugin that can autofill & autocomplete messages (https://github.com/a-riceeater/Autocomplete-BetterDiscord)
+<br><br>
+<u>QOL BetterDiscord Plugin</u> - A BetterDiscord plugin that combines many QOL plugins into one (https://github.com/a-riceeater/QOL-BetterDiscord-Plugin)
+<br><br>
+<u>Forge</u> - (h(ttps://github.com/a-riceeater/Forge)
 `
