c45191446275f3348b247186ae190781cdfad6d2
darthvader1925 <elijb12@gmail.com>
2024-08-05 09:59:20 -0700

    send file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
 })
 
 app.listen(7076, () => {
