52b973eb6cdd364bf8aedccb62f77868eb341989
darthvader1925 <elijb12@gmail.com>
2024-08-05 15:47:33 -0700

    404 route

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ app.get("/license", (req, res) => {
     res.sendFile(path.join(__dirname, "html", "license.html"))
 })
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', '404.html'));
+})
+
 app.listen(7076, () => {
     console.log('Listening on 7076')
 })
