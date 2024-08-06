2cd076bc2b492d52bdb3ee8c4fd142309bfcce9f
darthvader1925 <elijb12@gmail.com>
2024-08-05 17:00:20 -0700

    hamburger toggle

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,13 @@
 /*document.querySelector(".hc-about-btn").addEventListener("click", () => {
     window.location = "#about"
 })*/
+
+document.querySelector("#h_drop").addEventListener("click", (e) => {
+    document.querySelector("#h_drop").classList.add("hidden")
+    document.querySelector("#h_close").classList.remove("hidden")
+})
+
+document.querySelector("#h_close").addEventListener("click", (e) => {
+    document.querySelector("#h_close").classList.add("hidden")
+    document.querySelector("#h_drop").classList.remove("hidden")
+})
