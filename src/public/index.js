7bee4cffd1dcb5d11c0c48f7628dceb87636980c
darthvader1925 <elijb12@gmail.com>
2024-08-05 22:04:21 -0700

    view height

--- src/public/index.js
+++ src/public/index.js
@@ -2,12 +2,14 @@
     window.location = "#about"
 })*/
 
-document.querySelector("#h_drop").addEventListener("click", (e) => {
+document.querySelector("#h_drop").addEventListener("click", () => {
     document.querySelector("#h_drop").classList.add("hidden")
     document.querySelector("#h_close").classList.remove("hidden")
+    document.querySelector("#sticky-drop").style.display = "block"
 })
 
-document.querySelector("#h_close").addEventListener("click", (e) => {
+document.querySelector("#h_close").addEventListener("click", () => {
     document.querySelector("#h_close").classList.add("hidden")
     document.querySelector("#h_drop").classList.remove("hidden")
+    document.querySelector("#sticky-drop").style.display = "none"
 })
